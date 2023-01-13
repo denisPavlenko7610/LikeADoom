69fcb0a8974f793f640052354e0e0b7b48c76f2c
Nia <108984073+kawaii-Code@users.noreply.github.com>
2023-01-13 17:17:20 +0300

    Hello World logger

---
+++ Assets/Scripts/LikeADoom/Trainee/Logger.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+namespace LikeADoom.Trainee
+{
+    public class Logger : MonoBehaviour
+    {
+        private void Start()
+        {
+            Debug.Log("Hello, World!");
+        }
+    }
+}
