08ef75593533a658fc62f4d16fc85cada9e2e0b7
Nia <108984073+kawaii-Code@users.noreply.github.com>
2023-02-11 20:50:32 +0300

    refactor: Return interface instead of implementation

--- Assets/Scripts/LikeADoom/Bullet/BulletBuilder/IBulletBuilder.cs
+++ Assets/Scripts/LikeADoom/Bullet/BulletBuilder/IBulletBuilder.cs
@@ -4,8 +4,8 @@ namespace LikeADoom.Shooting.BulletBuilder
 {
     public interface IBulletBuilder
     {
-        BulletBuilder SetupBulletPosition(Transform spawnPoint);
-        BulletBuilder SetIsReleased(bool isReleased);
+        IBulletBuilder SetupBulletPosition(Transform spawnPoint);
+        IBulletBuilder SetIsReleased(bool isReleased);
         IBullet Build();
     }
 }
